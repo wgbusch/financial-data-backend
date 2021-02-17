522ab442c93b667896a9871694ed428deaa67c40
Werner Busch <w.g.busch@hotmail.com>
2021-02-16 21:40:17 -0300

    deleted cache and modified prcoofile

--- app/core.py
+++ app/core.py
@@ -17,20 +17,8 @@ from tqdm import tqdm
 
 from db import db
 
-#
-# MARKET_DATA_DIRECTORY = "data/market_data/"
-# HISTORICAL_DATA_DIRECTORY = "data/historical_data/"
-# OPTIONS_DATA_DIRECTORY = "data/options/"
-# COLUMNS_STATE_DIRECTORY = "data/columns_state/"
-
 model = ["Open", "High", "Low", "Close", "Adj Close", "Volume", "Symbol", "Name", "Is_ETF"]
 pd.options.mode.chained_assignment = None
-
-
-# def create_daily_file():
-#     df = pd.DataFrame([], columns=model)
-#     df.to_csv(MARKET_DATA_DIRECTORY + str(datetime.date.today()) + ".csv", index=False)
-
 
 class mainObj:
     SLICE = 100
@@ -44,72 +32,6 @@ class mainObj:
 
     def main_func(self):
         pass
-
-        # print("Downloading market data for today...")
-        # self.download_market_data()
-
-    # def initial_download(self, tick='1d'):
-    #     print("Downloading historic data...")
-    #     with parallel_backend('loky', n_jobs=multiprocessing.cpu_count()):
-    #         Parallel()(delayed(self.initial_download_for_ticker)(ticker, tick)
-    #                    for ticker in tqdm(self.list_of_tickers))
-    #
-    # def initial_download_for_ticker(self, ticker, tick='1d'):
-    #     currentDate = datetime.date.today() + datetime.timedelta(days=1)
-    #
-    #     file_name = HISTORICAL_DATA_DIRECTORY + ticker["symbol"] + "_" + tick + ".csv"
-    #     try:
-    #         f = open(file_name)
-    #     except IOError:
-    #         sys.stdout = open(os.devnull, "w")
-    #         data = yf.download(tickers=ticker["symbol"], end=currentDate, interval=tick)
-    #         data["Symbol"] = np.array(ticker["symbol"])
-    #         data["Name"] = np.array(ticker["name"])
-    #         data["Is_ETF"] = np.array(ticker["is_etf"])
-    #         sys.stdout = sys.__stdout__
-    #         data.to_csv(file_name)
-    #
-    # def download_market_data(self, tickers):
-    #     if not tickers:
-    #         tickers = self.list_of_tickers
-    #     currentDate = datetime.date.today()
-    #     file_name = MARKET_DATA_DIRECTORY + str(currentDate) + ".csv"
-    #     try:
-    #         f = open(file_name)
-    #     except IOError:
-    #         manager = multiprocessing.Manager()
-    #         market_data = manager.list()
-    #
-    #         with parallel_backend('loky', n_jobs=multiprocessing.cpu_count()):
-    #             Parallel()(delayed(self.get_today_values)(ticker, market_data, currentDate)
-    #                        for ticker in tqdm(tickers))
-    #
-    #         df = pd.concat(list(market_data))
-    #
-    #         df.to_csv(file_name)
-    #     pass
-    #
-    # def get_today_values(self, ticker, market_data, currentDate, interval='1d'):
-    #     sys.stdout = open(os.devnull, "w")
-    #     data = self.get_ticker(ticker["symbol"])
-    #     sys.stdout = sys.__stdout__
-    #     file_name = HISTORICAL_DATA_DIRECTORY + ticker["symbol"] + "_" + interval + ".csv"
-    #
-    #     df = pd.read_csv(file_name)
-    #     try:
-    #         if (len(data) > 0 and dt.strptime(df["Date"].iloc[-1], '%Y-%m-%d') < dt.strptime(str(currentDate),
-    #                                                                                          '%Y-%m-%d')):
-    #             df.append(data)
-    #     except Exception as e:
-    #         exc_type, exc_obj, exc_tb = sys.exc_info()
-    #         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    #         print(exc_type, fname, exc_tb.tb_lineno)
-    #         print("Error adding data to data frame, for filename " + file_name)
-    #     data["Symbol"] = np.array(ticker["symbol"])
-    #     data["Name"] = np.array(ticker["name"])
-    #     data["Is_ETF"] = np.array(ticker["is_etf"])
-    #     market_data.append(data)
-
     # 6
     def get_ticker(self, id):
         sys.stdout = open(os.devnull, "w")
