d92a56e7e22b76ad4abbb128df60e410e31d3c75
Werner Busch <w.g.busch@hotmail.com>
2021-03-16 21:41:00 -0300

    adding route to get tickers

--- app.py
+++ app.py
@@ -1,25 +1,37 @@
 import os
 import sys
 
+import flask_restful
 from flask import Flask, jsonify, Response, make_response, request, url_for
 from flask_cors import CORS
 from flask_restful import Resource, Api
 
-import core
-import options_logic
+import appLogic
 
 print('-----main running----')
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+PREFIX = '/api/v1'
 
 incomes = [
     {'description': 'salary', 'amount': 5000}
 ]
 
 
+@app.route(PREFIX+'/')
+def get_tickers():
+    tickers = request.args.get('tickers')
+    if (tickers):
+        tickers_list = tickers.split(',')
+        appLogic.get_tickers(tickers_list)
+    return jsonify(incomes)
+
+
 @app.route('/incomes')
 def get_incomes():
     return jsonify(incomes)
+
 
 @app.route('/incomes', methods=['POST'])
 def add_income():
